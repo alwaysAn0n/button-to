4c66e457c7a22afd0972249f4f75cfb6da191c36
@braindream <braindream@protonmail.com>
2018-10-27 17:46:46 +0200

    Update scripts/NearestButton.js

--- scripts/NearestButton.js
+++ scripts/NearestButton.js
@@ -2,7 +2,7 @@ var geolib = require('geolib');
 
 function NearestButton(userLocation, locations) {
 	var found = geolib.findNearest(userLocation, locations, 1)
-//	console.log(found);
+ //	console.log(found);
 	if(found['distance']<1000) {
 		return [found];
 	} else return [];
